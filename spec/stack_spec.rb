856f5840cf571c8eed7c03d8658c537345b77bdf
Soumya Veer <veer.soumya@gmail.com>
2018-05-24 15:05:24 -0700

    add test and method to initialize stack items

--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -1,3 +1,10 @@
-describe Stack do
+require_relative "../src/stack"
 
+describe Stack do
+  describe 'initialize' do
+    it 'creates an empty stack' do
+      stack = Stack.new()
+      expect(stack.items).to match([])
+    end
+  end
 end
--- src/stack.rb
+++ src/stack.rb
@@ -1,3 +1,8 @@
 class Stack
+  # methods: push, pop, peek, isEmpty, clear, size
+  attr_accessor :items
 
+  def initialize(items = [])
+    @items = items
+  end
 end
