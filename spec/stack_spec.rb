06cf51c8f2ab83c5b10c2af90efd968822b5cc5a
Soumya Veer <veer.soumya@gmail.com>
2018-05-24 15:25:11 -0700

    add tests for the expected behaviour of the stack methods

--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -1,10 +1,87 @@
 require_relative "../src/stack"
 
 describe Stack do
+  before do
+    @stack = Stack.new()
+  end
+
   describe 'initialize' do
     it 'creates an empty stack' do
-      stack = Stack.new()
-      expect(stack.items).to match([])
+      expect(@stack.items).to match([])
+    end
+  end
+
+  describe 'push' do
+    it 'adds a new item to the top of the stack' do
+      updated_stack = @stack.push(1)
+      expect(updated_stack).to match_array([1])
+    end
+
+    it 'adds second new item to the top of the stack' do
+      updated_stack = @stack.push(2)
+      expect(updated_stack).to match_array([1,2])
+    end
+
+    it 'adds third new item to the top of the stack' do
+      updated_stack = @stack.push(3)
+      expect(updated_stack).to match_array([1,2, 3])
+    end
+  end
+
+  describe 'pop' do
+    it 'removes the top item from the stack' do
+      updated_stack = @stack.pop
+      expect(updated_stack).to match_array([2,3])
+    end
+
+    it 'removes another item from the top of the stack' do
+      updated_stack = @stack.pop
+
+      expect(updated_stack).to match_array([3])
+    end
+  end
+
+  describe 'peek' do
+    it 'returns the top element of the stack' do
+      top_element = @stack.peek
+
+      expect(top_element).to eql(3)
+    end
+  end
+
+  describe 'isEmpty' do
+    it 'returns false is the stack is not empty' do
+      expect(@stack.isEmpty).to be_falsey
+    end
+
+    it 'returns true if the stack is empty' do
+      @stack.pop
+
+      expect(@stack.isEmpty).to be_truthy
+    end
+  end
+
+
+  describe 'clear' do
+    it 'clears the elements of the stack' do
+      @stack.push(4)
+      @stack.push(5)
+      @stack.push(6)
+
+      expect(@stack.isEmpty).to be_falsey
+      expect(@stack.clear).to match_array([])
+      expect(@stack.isEmpty).to be_truthy
+    end
+  end
+
+  describe 'print' do
+    it 'displays the elements of the stack' do
+      @stack.push(7)
+      @stack.push(8)
+      @stack.push(9)
+      @stack.push(10)
+
+      expect(@stack.print).to eql('7,8,9,10')
     end
   end
 end
