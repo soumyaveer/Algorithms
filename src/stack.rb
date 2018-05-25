0747ebb9bcd47fc7951e50ea2ed83e41b9fe3692
Soumya Veer <veer.soumya@gmail.com>
2018-05-25 11:06:56 -0700

    alphabetize methods and add comments to describe the behaviour of each method

--- src/stack.rb
+++ src/stack.rb
@@ -1,37 +1,45 @@
 class Stack
-  # methods: push, pop, peek, isEmpty, clear, size
+  # methods: push, pop, peek, isEmpty?, clear, size, print
   attr_accessor :items
 
+  # clear stack
+  def clear
+    @items = []
+  end
+
+  # create new stack
   def initialize(items = [])
     @items = items
   end
 
-  def push(element)
-    @items.push(element)
+  # verify if stack is empty
+  def isEmpty?
+    @items.empty?
   end
 
-  def pop
-    @items.pop
-  end
-
+  # returns the topmost element of the stack
   def peek
     last_element_index = @items.length - 1
     @items[last_element_index]
   end
 
-  def isEmpty?
-    @items.empty?
+  # remove an element from stack
+  def pop
+    @items.pop
   end
 
-  def clear
-    @items = []
-  end
-
-  def size
-    @items.length
-  end
-
+  # display elements in the stack
   def print
     @items.join(', ')
+  end
+
+  # add an element to the stack
+  def push(element)
+    @items.push(element)
+  end
+
+  # returns the number of elements in the stack
+  def size
+    @items.length
   end
 end
