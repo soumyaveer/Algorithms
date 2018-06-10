325eef66c1d37f29346ae006dc891d4b4374f4b4
Soumya Veer <veer.soumya@gmail.com>
2018-06-10 15:08:11 -0700

    add insert, to_s and size method with insert method tests

--- spec/LinkedList/doubly_linked_list_spec.rb
+++ spec/LinkedList/doubly_linked_list_spec.rb
@@ -1,4 +1,4 @@
-require_relative "../../../src/LinkedList/node"
+require_relative "../../src/LinkedList/node"
 require_relative "../../src/LinkedList/doubly_linked_list"
 
 describe LinkedList::DoublyLinkedList do
@@ -16,13 +16,22 @@ describe LinkedList::DoublyLinkedList do
 
   describe 'insert' do
     it 'inserts a new element at specified position' do
+      @linked_list.append('B')
+      @linked_list.append('C')
+      @linked_list.append('E')
 
+      expect(@linked_list.size).to eql(4)
+
+      @linked_list.insert(3, 'D')
+
+      expect(@linked_list.size).to eql(5)
+      expect(@linked_list.to_s).to eql("A, B, C, D, E")
     end
   end
 
   describe 'remove_at' do
     it 'removes an item from a specified position in the list' do
-      
+
     end
   end
 end
--- src/LinkedList/doubly_linked_list.rb
+++ src/LinkedList/doubly_linked_list.rb
@@ -2,10 +2,68 @@ module LinkedList
   class DoublyLinkedList
     attr_accessor :head
 
-    def initialize(element = nil)
-      @head = LinkedList::Node.new(element, nil, nil )
+    def initialize(data = nil)
+      @head = LinkedList::Node.new(data, nil, nil )
     end
 
+    def append(element)
+      current_node = @head
+      previous_node = nil
 
+      while current_node.next?
+        previous_node = current_node.previous_pointer
+        current_node = current_node.next_pointer
+      end
+
+      new_node = LinkedList::Node.new(element, nil)
+      current_node.next_pointer = new_node
+      current_node.previous_pointer = previous_node
+    end
+
+    def size
+      current_node = @head
+      index = 1
+
+      while current_node.next?
+        index += 1
+        current_node = current_node.next_pointer
+      end
+
+      index
+    end
+
+    def to_s
+      current_node = @head
+      elements = []
+
+      while current_node
+        elements.push(current_node.data)
+        current_node = current_node.next_pointer
+      end
+
+      elements.join(", ")
+    end
+
+    def insert(position, element)
+      current_node = @head
+      index = 0
+
+      previous_node = current_node
+      next_node = current_node
+      new_node = LinkedList::Node.new(element, nil)
+
+      while current_node
+        if index == position - 1
+          previous_node = current_node
+          next_node = current_node.next_pointer
+        end
+        index += 1
+        current_node = current_node.next_pointer
+      end
+
+      new_node.next_pointer  = next_node
+      new_node.previous_pointer = previous_node
+      previous_node.next_pointer = new_node
+    end
   end
 end
