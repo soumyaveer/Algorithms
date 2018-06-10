22968ff20b3bd62be653751a870784cf4cbb3d23
Soumya Veer <veer.soumya@gmail.com>
2018-06-10 16:05:32 -0700

    add size method

--- src/LinkedList/circular_singly_linked_list.rb
+++ src/LinkedList/circular_singly_linked_list.rb
@@ -9,6 +9,27 @@ module LinkedList
     def empty?
       @head.data.nil?
     end
+
+    def size
+      current_node = @head
+      index = 1
+      repeated_node = 0
+      nodes = []
+
+      while current_node && repeated_node < 1
+        nodes.push(current_node)
+
+        if nodes.include?(current_node)
+          repeated_node += 1
+          index
+        else
+          repeated_node = 0
+          index += 1
+        end
+        current_node.next_pointer
+      end
+      index
+    end
   end
 end
 
