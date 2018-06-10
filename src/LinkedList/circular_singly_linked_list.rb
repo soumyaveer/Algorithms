340d429f26aac6cbc60ba7e22318ca65dd37b570
Soumya Veer <veer.soumya@gmail.com>
2018-06-10 15:55:03 -0700

    add initialize and empty? methods

--- src/LinkedList/circular_singly_linked_list.rb
+++ src/LinkedList/circular_singly_linked_list.rb
@@ -1,6 +1,14 @@
 module LinkedList
   class CircularSinglyLinkedList
+    attr_accessor :head
 
+    def initialize(data = nil)
+      @head = LinkedList::Node.new(data, nil)
+    end
+
+    def empty?
+      @head.data.nil?
+    end
   end
 end
 
