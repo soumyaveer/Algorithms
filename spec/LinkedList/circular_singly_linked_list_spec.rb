481953c6e13031ecf8218af7a7b6fd406049a833
Soumya Veer <veer.soumya@gmail.com>
2018-06-11 14:06:05 -0700

    add insert and append methods and respective tests

--- spec/LinkedList/circular_singly_linked_list_spec.rb
+++ spec/LinkedList/circular_singly_linked_list_spec.rb
@@ -15,7 +15,10 @@ describe LinkedList::CircularSinglyLinkedList do
 
   describe 'size' do
     it 'returns number of elements in the linked list' do
-      expect(@linked_list.size).to eql(1)
+      @linked_list.append('B')
+
+      p @linked_list.inspect
+      expect(@linked_list.size).to eql(2)
     end
   end
 
@@ -30,10 +33,37 @@ describe LinkedList::CircularSinglyLinkedList do
     end
   end
 
+  # describe 'to_s' do
+  #   it 'returns the elements in the linked list' do
+  #     @linked_list.append('B')
+  #     # @linked_list.append('C')
+  #
+  #     expect(@linked_list.to_s).to eql("A, B")
+  #   end
+  # end
+
   describe 'insert' do
     it 'inserts an element in the linked list at the given position' do
+      @linked_list.append('B')
+      @linked_list.append('C')
+      @linked_list.append('E')
 
+      expect(@linked_list.size).to eql(4)
+
+      @linked_list.insert('D', 3)
+       p @linked_list.inspect
+      expect(@linked_list.size).to eql(5)
+      # expect(@linked_list.to_s).to eql("A, B")
     end
   end
 
+  describe 'append' do
+    it 'adds a new node at the end of the linked list' do
+      @linked_list.append('B')
+      @linked_list.append('C')
+
+      expect(@linked_list.size).to eql(3)
+      # expect(@linked_list.to_s).to eql("A, B, C")
+    end
+  end
 end
--- src/LinkedList/circular_singly_linked_list.rb
+++ src/LinkedList/circular_singly_linked_list.rb
@@ -2,6 +2,19 @@ module LinkedList
   class CircularSinglyLinkedList
     attr_accessor :head
 
+    def append(element)
+      current_node = @head
+      p "Head node #{@head.inspect}"
+
+      while current_node.next? && current_node.next_pointer != @head
+        current_node = current_node.next_pointer
+      end
+
+      new_node = LinkedList::Node.new(element, nil)
+      new_node.next_pointer = @head
+      current_node.next_pointer = new_node
+    end
+
     def initialize(data = nil)
       @head = LinkedList::Node.new(data, nil)
     end
@@ -13,22 +26,46 @@ module LinkedList
     def size
       current_node = @head
       index = 1
-      repeated_node = 0
-      nodes = []
 
-      while current_node && repeated_node < 1
-        nodes.push(current_node)
-
-        if nodes.include?(current_node)
-          repeated_node += 1
-          index
-        else
-          repeated_node = 0
-          index += 1
-        end
-        current_node.next_pointer
+      while current_node.next? && current_node.next_pointer != @head
+        index += 1
+        current_node = current_node.next_pointer
       end
+
       index
+    end
+    #
+    # def to_s
+    #   current_node = @head
+    #   elements = []
+    #
+    #   while current_node.next?
+    #     elements.push(current_node.data)
+    #     p elements
+    #     current_node = current_node.next_pointer
+    #   end
+    #
+    #   elements.join(", ")
+    # end
+
+    def insert(element, position)
+      current_node = @head
+      index = 0
+      previous_node = nil
+      next_node = nil
+
+      while current_node.next_pointer
+        if index == position - 1
+          previous_node = current_node
+          next_node = current_node.next_pointer
+          break
+        end
+        index += 1
+        current_node = current_node.next_pointer
+      end
+
+      new_node = LinkedList::Node.new(element, next_node)
+      previous_node.next_pointer = new_node
     end
   end
 end
