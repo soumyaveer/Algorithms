b73dfcc5ba92bbf6fbac8a233259ec4ec8eb767f
Soumya Veer <veer.soumya@gmail.com>
2018-06-11 15:16:23 -0700

    add empty? and initialize methods

--- src/LinkedList/circular_doubly_linked_list.rb
+++ src/LinkedList/circular_doubly_linked_list.rb
@@ -1,4 +1,15 @@
 module LinkedList
   class CircularDoublyLinkedList
+    attr_accessor :head
+
+    def initialize(data = nil)
+      @head = LinkedList::Node.new(data, nil, nil)
+    end
+
+    def empty?
+      @head.data.nil?
+    end
+
+    
   end
 end
