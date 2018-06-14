68fde1ce5eb4b7f47d35be88572ffeafbdde7d6f
Soumya Veer <veer.soumya@gmail.com>
2018-06-14 13:01:44 -0700

    add Set#class and set_spec file

---
+++ spec/set_spec.rb
@@ -0,0 +1,13 @@
+require_relative "../src/set"
+
+describe Set do
+  before do
+    @set = Set.new
+  end
+
+  describe 'initialize' do
+    it 'creates a new empty set' do
+      
+    end
+  end
+end
