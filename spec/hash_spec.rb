357acc75ff1ce9645bb9344d4016cc19466c6a04
Soumya Veer <veer.soumya@gmail.com>
2018-06-16 17:00:48 -0700

    add tests for Hash#Class methods

--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -10,4 +10,52 @@ describe Hash do
       expect(@hash.empty?).to be_truthy
     end
   end
+
+  describe 'put' do
+    it 'adds a new item to the hash table' do
+      @hash.put("Gandalf", "gandalf_the_grey@wizard.com")
+      @hash.put("Legolas", "legolas@elf.com")
+      @hash.put("Aragorn", "strider@gondor.com")
+      @hash.put("Frodo", "frodo_baggins@shire.com")
+
+      expect(@hash.size).to eql(4)
+    end
+  end
+
+  describe 'remove' do
+    it 'removes the value from the hash table using the key' do
+      @hash.put("Gandalf", "gandalf_the_grey@wizard.com")
+      @hash.put("Legolas", "legolas@elf.com")
+      @hash.put("Aragorn", "strider@gondor.com")
+      @hash.put("Frodo", "frodo_baggins@shire.com")
+
+      expect(@hash.size).to eql(4)
+
+      @hash.remove("Legolas")
+
+      expect(@hash.size).to eql(3)
+    end
+  end
+
+  describe 'get' do
+    it 'returns a specific value searched by the key' do
+      @hash.put("Gandalf", "gandalf_the_grey@wizard.com")
+      @hash.put("Legolas", "legolas@elf.com")
+      @hash.put("Aragorn", "strider@gondor.com")
+      @hash.put("Frodo", "frodo_baggins@shire.com")
+
+      expect(@hash.size).to eql(4)
+
+      expect(@hash.get("Aragorn")).to eql("strider@gondor.com")
+    end
+  end
+
+  describe 'size' do
+    it 'returns the number of elements in the hash table' do
+      @hash.put("Gandalf", "gandalf_the_grey@wizard.com")
+      @hash.put("Legolas", "legolas@elf.com")
+
+      expect(@hash.size).to eql(2)
+    end
+  end
 end
