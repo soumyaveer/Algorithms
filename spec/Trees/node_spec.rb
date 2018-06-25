e4ef3b9638a836deca6072610858493ee63a606f
Soumya Veer <veer.soumya@gmail.com>
2018-06-25 12:21:45 -0700

    add Node#Class and tests

--- spec/Trees/node_spec.rb
+++ spec/Trees/node_spec.rb
@@ -2,8 +2,50 @@ require_relative "../../src/Trees/node"
 
 describe Trees::Node do
   before do
-    @node = Node.new(11)
+    @node = Trees::Node.new(11)
+  end
+
+  describe 'initialize' do
+    it 'creates a new node with the specified data' do
+      expect(@node.empty?).to be_falsey
+    end
+  end
+
+  describe 'setter' do
+    context 'left=' do
+      it 'assigns the node to the left of the old node' do
+        left_node = Trees::Node.new(7)
+        @node.left = left_node
+        expect(@node.left).to eql(left_node)
+        end
+      end
+
+    context 'right=' do
+      it 'assigns the node to the right of the old node' do
+        right_node = Trees::Node.new(27)
+        @node.right = right_node
+        expect(@node.right).to eql(right_node)
+      end
+    end
   end
 
 
+
+  describe 'getter' do
+    context 'left' do
+      it 'returns the left node' do
+        left_node = Trees::Node.new(7)
+        @node.left = left_node
+        expect(@node.left).to eql(left_node)
+      end
+    end
+
+    context 'right' do
+      it 'returns the right node' do
+        right_node = Trees::Node.new(27)
+        @node.right = right_node
+        expect(@node.right).to eql(right_node)
+      end
+    end
+  end
 end
--- src/Trees/node.rb
+++ src/Trees/node.rb
@@ -1,5 +1,31 @@
 module Trees
   class Node
+    attr_accessor :key
 
+    def initialize(key, right = nil, left = nil)
+      @key = key
+      @right = right
+      @left = left
+    end
+
+    def right=(right_node)
+      @right = right_node
+    end
+
+    def right
+      @right
+    end
+
+    def left=(left_node)
+      @left = left_node
+    end
+
+    def left
+      @left
+    end
+
+    def empty?
+      @key.nil?
+    end
   end
 end
