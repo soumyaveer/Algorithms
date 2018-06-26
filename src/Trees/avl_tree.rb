a911de431dea33eb8ca6c7122d9bc6fe9b7acd6f
Soumya Veer <veer.soumya@gmail.com>
2018-06-26 12:15:17 -0700

    add pre_order_traversal and to_s method to AVLTrees#Class

--- src/Trees/avl_tree.rb
+++ src/Trees/avl_tree.rb
@@ -24,5 +24,22 @@ module Trees
       left_level > right_level ? left_level : right_level
     end
 
+    def pre_order_traversal(node)
+      values = []
+
+      if node.nil?
+        return []
+      else
+        values.push(node.key)
+        values.push(pre_order_traversal(node.left))
+        values.push(pre_order_traversal(node.right))
+      end
+
+      to_s(values)
+    end
+
+    def to_s(keys)
+      keys.flatten.join(" ")
+    end
   end
 end
