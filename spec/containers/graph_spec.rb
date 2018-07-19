3c00abefbf204dddee81a2c988b893c3bb6a95cf
Soumya Veer <veer.soumya@gmail.com>
2018-07-19 13:51:07 -0700

    add tests for empty?#method, size_of_vertices#method, add_vetrex#method, add_edge#method and to_s#method

--- spec/containers/graph_spec.rb
+++ spec/containers/graph_spec.rb
@@ -8,6 +8,92 @@ module Containers
 
     describe 'initialize' do
       it 'creates a new graph data structure' do
+        expect(@graph.size_of_vertices).to eql(0)
+        expect(@graph.empty?).to be_truthy
+      end
+    end
+
+    describe 'empty?' do
+      it 'returns true if the graph has no vertices' do
+        expect(@graph.empty?).to be_truthy
+      end
+
+      it 'returns false if the graph has vertex' do
+        @graph.add_vertex('A')
+        expect(@graph.empty?).to be_falsy
+      end
+    end
+
+    describe 'size_of_vertices' do
+      it 'returns the number of vertices in the graph' do
+        @graph.add_vertex('A')
+        @graph.add_vertex('B')
+
+        expect(@graph.size_of_vertices).to eql(2)
+      end
+    end
+
+    describe 'add_vertex' do
+      it 'adds a new vertex to the graph' do
+        @graph.add_vertex('A')
+        @graph.add_vertex('B')
+
+        expect(@graph.empty?).to be_falsy
+        expect(@graph.size_of_vertices).to eql(2)
+      end
+    end
+
+    describe 'add_edge' do
+      it 'adds edges to the vertices' do
+        @graph.add_vertex('A')
+        @graph.add_vertex('B')
+        @graph.add_vertex('C')
+
+        @graph.add_edge('A', 'B')
+        @graph.add_edge('A', 'C')
+
+        expect(@graph.empty?).to be_falsey
+        expect(@graph.size_of_vertices).to eql(3)
+
+        expect(@graph.to_s).to eql("A -> B C")
+      end
+    end
+
+    describe 'to_s' do
+      it 'displays the graph' do
+        @graph.add_vertex('A')
+        @graph.add_vertex('B')
+        @graph.add_vertex('C')
+        @graph.add_vertex('D')
+        @graph.add_vertex('E')
+        @graph.add_vertex('F')
+        @graph.add_vertex('G')
+        @graph.add_vertex('H')
+        @graph.add_vertex('I')
+
+        @graph.add_edge('A', 'B')
+        @graph.add_edge('A', 'C')
+        @graph.add_edge('A', 'D')
+        @graph.add_edge('C', 'D')
+        @graph.add_edge('C', 'G')
+        @graph.add_edge('D', 'G')
+        @graph.add_edge('D', 'H')
+        @graph.add_edge('B', 'E')
+        @graph.add_edge('B', 'F')
+        @graph.add_edge('E', 'I')
+
+        expect(@graph.empty?).to be_falsey
+        expect(@graph.size_of_vertices).to eql(9)
+
+        expect(@graph.to_s).to eql("A -> B C D \n
+                                    B -> A E F \n
+                                    C -> A D G \n
+                                    D -> A C G H \n
+                                    E -> B I \n
+                                    F -> B \n
+                                    G -> C D\n
+                                    H -> D \n
+                                    I -> E")
       end
     end
   end
--- src/containers/graph.rb
+++ src/containers/graph.rb
@@ -3,9 +3,21 @@ module Containers
   class Graph
     attr_accessor :vertices, :adj_list
 
-    def initialize(vertices = [])
-      @vertices = vertices
+    def initialize
+      @vertices = []
       @adj_list = Dictionary.new
+    end
+
+    def empty?
+      vertices.empty?
+    end
+
+    def size_of_vertices
+      vertices.count
+    end
+
+    def add_vertex(vertex)
+
     end
   end
 end
